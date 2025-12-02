8a7857d4ec616a73dcca24bd4fd09d7b6564afae
Soham Mukherjee <iamsoham2011@gmail.com>
2025-12-03 03:17:53 +0530

    deployment modifications

--- main.py
+++ main.py
@@ -477,4 +477,11 @@ def not_found(e):
 
 # ---- Run ----
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=int(os.getenv("PORT", 5000)))
+    import eventlet
+    eventlet.monkey_patch()
+
+    socketio.run(
+        app,
+        host="0.0.0.0",
+        port=int(os.getenv("PORT", 5000))
+    )
